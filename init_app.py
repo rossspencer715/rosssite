a084b6d8e5301518e907107c2b6f1cb714bf2571
Ross Spencer <ross.spencer715@gmail.com>
2022-08-26 18:35:01 -0400

    added Black and flake8 as pre-commit hooks for formatting and linting

--- init_app.py
+++ init_app.py
@@ -1,23 +1,22 @@
 from flask import Flask, render_template
-import os
 
 app = Flask(__name__)
 
+
 def get_interests():
     interests = [
-        'computer science',
-        'coffee'
-        'yoga',
-        'pure math',
-        'RuPaul\'s Drag Race',
-        'the color orange'
+        "computer science",
+        "coffee" "yoga",
+        "pure math",
+        "RuPaul's Drag Race",
+        "the color orange",
     ]
     return interests
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html', interests=get_interests())
+    return render_template("index.html", interests=get_interests())
 
 
 if __name__ == "__main__":
