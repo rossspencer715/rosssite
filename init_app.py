d0964b5b7984e5407b4589a76b00e746f044391f
Ross Spencer <ross.spencer715@gmail.com>
2022-08-26 18:44:29 -0400

    merge master into main

---
+++ init_app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+def get_interests():
+    interests = [
+        "computer science",
+        "coffee" "yoga",
+        "pure math",
+        "RuPaul's Drag Race",
+        "the color orange",
+    ]
+    return interests
+
+
+@app.route("/")
+def home():
+    return render_template("index.html", interests=get_interests())
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
