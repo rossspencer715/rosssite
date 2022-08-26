bd2ac095116a9c74430dafb8ea11b805f8b5abd9
Ross Spencer <ross.spencer715@gmail.com>
2022-08-26 18:16:57 -0400

    added interests

--- init_app.py
+++ init_app.py
@@ -3,10 +3,21 @@ import os
 
 app = Flask(__name__)
 
+def get_interests():
+    interests = [
+        'computer science',
+        'coffee'
+        'yoga',
+        'pure math',
+        'RuPaul\'s Drag Race',
+        'the color orange'
+    ]
+    return interests
+
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', interests=get_interests())
 
 
 if __name__ == "__main__":
